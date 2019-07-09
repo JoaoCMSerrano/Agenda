956f71e64678e20cdf2c26a2c042e4d253dd1852
JoaoCMSerrano <joaocmserrano@users.noreply.github.com>
2019-07-10 00:02:33 +0100

    Actualizacao do model TipoServico

--- Material/agenda-digital-model/src/main/java/com/everis/academia/java/agendadigital/model/TipoServico.java
+++ Material/agenda-digital-model/src/main/java/com/everis/academia/java/agendadigital/model/TipoServico.java
@@ -1,8 +1,9 @@
 package com.everis.academia.java.agendadigital.model;
 
 public class TipoServico {
+	
 	private Short codigo;
-	private String descricao;
+		private String descricao;
 
 	public TipoServico() {
 		super();
@@ -33,7 +34,32 @@ public class TipoServico {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TipoServico other = (TipoServico) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
 	@Override
 	public String toString() {
 		return "TipoServico [codigo=" + codigo + ", descricao=" + descricao + "]";
